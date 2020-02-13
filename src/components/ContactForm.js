3f7272fc0a67eaab8e00b4bbf0097f6876b8caf7
Aaron Beasley <aaroncb@aaroncb.com>
2020-02-13 12:36:17 -0600

    ADD Axios && Refactor ContactForm && Fetch Data with Axios
    - Added axios to the project for ease of use when updating local state
    - Refactored ContactForm to break logic into seperate concerns
    - Fetching response from data after post and updating state accordingly

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
+import axios from 'axios';
 
 const ContactForm = () => {
   const [data, setData] = useState();
@@ -8,21 +9,45 @@ const ContactForm = () => {
   });
 
   const onSubmit = data => {
-    setData(data);
+    // This only updates the local state. Replaced this with a response from a server
+    // setData(data);
+    postReqRes(data);
   };
 
-  // XHR Implementation rather than bringing in Axios
-  const xhr = new XMLHttpRequest()
-// Add event listener for when the server sends a resopnse
-  xhr.addEventListener('load', () => {
-    console.log(xhr.responseText)
-  })
-  // Tell xhr we will be making a POST 
-  xhr.open('POST', 'https://reqres.in/api/users')
+//   XHR Implementation rather than bringing in Axios
+
+//   const xhr = new XMLHttpRequest()
+
+//   Add event listener for when the server sends a resopnse
+//   xhr.addEventListener('load', () => {
+//     console.log(xhr.responseText)
+//   })
+
+//   Tell xhr we will be making a POST 
+//   xhr.open('POST', 'https://reqres.in/api/users')
 
   const postReqRes = data => {
     console.log(data)
-    
+    // xhr.send(JSON.stringify({
+    //   "name": "morpheus",
+    //   "job": "leader"
+    // }))
+    axios.post('https://reqres.in/api/users', data)
+    // Made the success call a function to handle data, and to also update the form to the correct state
+      .then(res => updateData(res.data))
+      .catch(err => console.log(err))
+  }
+
+  // This sets the local state to have to res.data, and also resets the form fields to their initial state
+  const updateData = data => {
+    console.log(data)
+    setData(data)
+    reset({
+      name: '',
+      lastName: '',
+      email: '',
+      message: ''
+    })
   }
 
   useEffect(() =>{
@@ -35,8 +60,7 @@ const ContactForm = () => {
         <div data-testid="name-element">
           <label htmlFor="firstName">First Name*</label>
           <input
-            name="firstName"
-            placeholder="bill"
+            name="name"
             ref={register({ required: true })}
           />
           {errors.firstName && (
@@ -48,7 +72,6 @@ const ContactForm = () => {
           <label htmlFor="lastName">Last Name*</label>
           <input
             name="lastName"
-            placeholder="luo"
             ref={register({ required: true })}
           />
           {errors.lastName && (
@@ -69,6 +92,7 @@ const ContactForm = () => {
           <label htmlFor="message">Message</label>
           <textarea name="message" ref={register({ required: false })} />
         </div>
+        {/* This data is empty on app load, then when the user makes a post request it will update on success */}
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
             {JSON.stringify(data, null, 2)}
